5aa122ec9d90b0605d1e12331f1f95a6f9d1628d
laiba jawed <laib.a.jaweddd@gmail.com>
2025-03-26 21:38:11 +0500

    complete assignments

---
+++ Online-Class-Projects/001_basics/01_basics/04_random_numbers.py
@@ -0,0 +1,15 @@
+import random
+
+N_NUMBERS: int = 10
+MIN_VALUE: int = 1
+MAX_VALUE: int = 100
+
+def main():
+    """
+    Generates and prints 10 random numbers in the range 1 to 100.
+    """
+    for _ in range(N_NUMBERS):
+        print(random.randint(MIN_VALUE, MAX_VALUE), end=" ")
+
+if __name__ == '__main__':
+    main()
